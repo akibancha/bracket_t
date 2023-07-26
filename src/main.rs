78aa2ac3779570fe38386c99788056cd0e5565ad
akibancha <sesametea@riseup.net>
2023-07-26 22:42:45 +0200

    2.2.2

--- src/main.rs
+++ src/main.rs
@@ -113,10 +113,13 @@ impl GameState for State {
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Pos>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TileType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(79, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(79, max(0, pos.y + delta_y));        }
     }
 }
 
